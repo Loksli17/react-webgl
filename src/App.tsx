6b83cf66d6f613e029820d33bad07a4ea7b41354
Loksli17 <ami0504@mail.ru>
2024-09-20 19:15:32 +0300

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,50 @@
-import './App.css';
-
+import Button from './ui/Button';
+import Header from './ui/Header';
 
 
 function App() 
 {
+    
+    const createShader: () => void = () => 
+    {
+        //todo call service
+        console.log("create Shader");
+    }
+    
+
     return (
         
-        <div className="app">
+        <div className="app py-10 grid gap-10">
+            
+            <div className='px-10 w-full border-box'>
 
-            <h1>Webgl2 React application</h1>
+                <Header content='Webgl2 react application'/>
+
+            </div>
+
+            <div className='px-10 flex gap-6 flex-wrap w-full border-box'>
+                
+                <Button
+                    className='text-lg font-medium
+                        px-4 
+                        py-4 
+                        bg-cyan-500 
+                        hover:bg-cyan-600 
+                        transition
+                        rounded-md
+                        text-white
+                    '
+                    onClick={createShader} 
+                    content={'Add shader'}
+                />
+
+                <Button 
+                    className='px-4 py-4 bg-cyan-100 text-lg rounded-md' 
+                    content='Hotkeys list'
+                />
+            
+            </div>
+
 
         </div>
     );
